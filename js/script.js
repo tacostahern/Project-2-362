504d5274ae2955449b8febc732e4a1c008ab2401
tacostahern <35584612+tacostahern@users.noreply.github.com>
2020-04-07 15:10:16 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function validateForm() {
+  //Form validation for first name field
+  var first = document.forms["myForm"]["firstname"].value;
+  if (first == "") {
+    alert(" First Name must be filled out");
+    return false;
+  }
+  //Form Validation for lastname field
+  var last = document.forms["myForm"]["lastname"].value;
+  if (last == "") {
+    alert("Last name must be filled out");
+    return false;
+  }
+
+  //Form validation for Email field
+  var email = document.forms["myForm"]["email"].value;
+  if (email == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  //Form validation for Phone number field
+  var phone = document.forms["myForm"]["phone"].value;
+  if (phone == "") {
+    alert("Phone Number must be filled out");
+    return false;
+  }
+}
